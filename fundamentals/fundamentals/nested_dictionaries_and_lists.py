7bba7f843f2a0d292905597d074e4807e6055856
Michael Natter <natter.michael@gmail.com>
2022-07-06 19:59:53 -0600

    second commit

--- fundamentals/fundamentals/nested_dictionaries_and_lists.py
+++ fundamentals/fundamentals/nested_dictionaries_and_lists.py
@@ -18,3 +18,57 @@ x[1][0] = 15
 students[0]["last_name"] = "Bryant"
 sports_directory["soccer"][0] = "Andres"
 z[0]["y"] = 30
+
+print(x)
+print(students)
+print(sports_directory)
+print(z)
+
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+
+def iterateDictionary(students):
+    for i in students:
+        for each_key in i:
+            print(each_key, i[each_key])
+
+
+iterateDictionary(students)
+
+
+# print(students[1]["first_name"])
+
+
+def iterateDictionary2(key_name, some_list):
+    for i in range(0, len(some_list)):
+        print(some_list[i][key_name])
+
+
+iterateDictionary2('first_name', students)
+iterateDictionary2("last_name", students)
+
+
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists,
+# prints the name of each key along with the size of its list, and then prints the associated
+# values within each key's list. For example:
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+# print(dojo["locations"])
+# print(len(dojo["locations"]))
+
+
+def printInfo(dictionary):
+    for key in dictionary:
+        print(len(dictionary[key]), key, dictionary[key])
+
+
+printInfo(dojo)
