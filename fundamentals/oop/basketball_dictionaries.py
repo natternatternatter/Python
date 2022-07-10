bbf16d6a7d68dba0ef31fa06705bf0be07c35bd6
Michael Natter <natter.michael@gmail.com>
2022-07-10 17:10:38 -0600

    first commit

---
+++ fundamentals/oop/basketball_dictionaries.py
@@ -0,0 +1,53 @@
+class Player:
+    def __init__(self, player):
+        self.name = player["name"]
+        self.age = player["age"]
+        self.position = player["position"]
+        self.team = player["position"]
+
+    @classmethod
+    def get_team(cls, team_list):
+        new_team = []
+        for player in players:
+            new_team.append(Player(player))
+        return new_team
+
+
+players = [
+    {
+        "name": "Kevin Durant",
+        "age": 34,
+        "position": "small forward",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum",
+        "age": 24,
+        "position": "small forward",
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving",
+        "age": 32,
+        "position": "Point Guard",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard",
+        "age": 33,
+        "position": "Point Guard",
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid",
+        "age": 32,
+        "position": "Power Foward",
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
